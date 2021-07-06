ff34512cb2e4813b6e4f47c5d8e8c16b9ddb9276
Bat-erdene.A <baterdene.a@ictgroup.mn>
2021-07-06 15:39:16 +0800

    huwin medelel page nemev

--- src/pages/admin/profile/index.js
+++ src/pages/admin/profile/index.js
@@ -1,17 +1,31 @@
 import { Menu } from 'antd';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import ProfileLayout from "@components/layouts/ProfileLayout";
+import { Modal, Button } from 'antd';
+import { useContext, useState } from 'react';
 
 const { SubMenu } = Menu;
 
 const Profile = () => {
-    const handleClick = (e) => {
-        console.log('click ', e);
-    };
+    const [visible, setVisible] = useState(false);
 
     return (
         <ProfileLayout className="main-content-no-back">
-            aaaaaaa
+            <Button onClick={() => setVisible(true)}>
+                Open Modal of 1000px width
+            </Button>
+            <Modal
+                title="Modal 1000px width"
+                centered
+                visible={visible}
+                onOk={() => setVisible(false)}
+                onCancel={() => setVisible(false)}
+                width={1000}
+            >
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+            </Modal>
         </ProfileLayout>
     );
 }
---
+++ src/pages/admin/profile/verify.js
@@ -0,0 +1,12 @@
+import ProfileLayout from "@components/layouts/ProfileLayout";
+
+const verify = () => {
+
+    return (
+        <ProfileLayout className="main-content-no-back">
+         Хувийн мэдээлэл бүртгүүлэх
+        </ProfileLayout>
+    );
+}
+
+export default verify;
