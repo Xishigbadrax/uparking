98174953d58bfa4480ab7b1848ae4867be664fbc
Bat-erdene.A <baterdene.a@ictgroup.mn>
2021-07-30 17:36:55 +0800

    auth && fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const path = require('path')
 
 module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  },
   // i18n: {
   //   locales: ['mn_MN', 'en-US'],
   //   defaultLocale: 'mn_MN',
