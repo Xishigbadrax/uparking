145657d25ddaa2b6798c27275bb95308b0565934
Ganbat B <ganbat.b@ictgroup.mn>
2021-08-23 14:54:42 +0800

    fixes11

--- src/components/DayNightColumn/index.js
+++ src/components/DayNightColumn/index.js
@@ -1,82 +1,50 @@
-import { PlusOutlined, MinusOutlined } from "@ant-design/icons";
+import {
+    PlusOutlined,
+    MinusOutlined,
+} from '@ant-design/icons';
 const DayNightColumn = () => {
-  return (
-    <div className="daynighttext">
-      <ul>
-        <li className="item">
-          <ul>
-            <li>
-              <PlusOutlined /> Өдөр
-            </li>
-            <li>
-              <MinusOutlined />
-              Шөнө
-            </li>
-          </ul>
-        </li>
-        <li className="item">
-          <ul>
-            <li>
-              <PlusOutlined />
-              Өдөр
-            </li>
-            <li>
-              <MinusOutlined />
-              Шөнө
-            </li>
-          </ul>
-        </li>
-        <li className="item">
-          <ul>
-            <li>
-              <PlusOutlined />
-              Өдөр
-            </li>
-            <li>
-              <MinusOutlined />
-              Шөнө
-            </li>
-          </ul>
-        </li>
-        <li className="item">
-          <ul>
-            <li>
-              <PlusOutlined />
-              Өдөр
-            </li>
-            <li>
-              <MinusOutlined />
-              Шөнө
-            </li>
-          </ul>
-        </li>
-        <li className="item">
-          <ul>
-            <li>
-              <PlusOutlined />
-              Өдөр
-            </li>
-            <li>
-              <MinusOutlined />
-              Шөнө
-            </li>
-          </ul>
-        </li>
-        <li className="item">
-          <ul>
-            <li>
-              <PlusOutlined />
-              Өдөр
-            </li>
-            <li>
-              <MinusOutlined />
-              Шөнө
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  );
-};
+    return (
+        <div className="daynighttext">
+            <ul>
+                <li className="item">
+                    <ul>
+                        <li><PlusOutlined /> Өдөр</li>
+                        <li><MinusOutlined />Шөнө</li>
+                    </ul>
+                </li>
+                <li className="item" >
+                    <ul>
+                        <li><PlusOutlined />Өдөр</li>
+                        <li><MinusOutlined />Шөнө</li>
+                    </ul>
+                </li>
+                <li className="item">
+                    <ul>
+                        <li><PlusOutlined />Өдөр</li>
+                        <li><MinusOutlined />Шөнө</li>
+                    </ul>
+                </li>
+                <li className="item" >
+                    <ul>
+                        <li><PlusOutlined />Өдөр</li>
+                        <li><MinusOutlined />Шөнө</li>
+                    </ul>
+                </li>
+                <li className="item" >
+                    <ul>
+                        <li><PlusOutlined />Өдөр</li>
+                        <li><MinusOutlined />Шөнө</li>
+                    </ul>
+                </li>
+                <li className="item">
+                    <ul>
+                        <li><PlusOutlined />Өдөр</li>
+                        <li><MinusOutlined />Шөнө</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    )
+}
 
 export default DayNightColumn;
