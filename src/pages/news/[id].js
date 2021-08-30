d12646dd8f7c2a28889895cece854f89abbb7310
Khishigbadrakh <xishigbadrax.enhtur@gmail.com>
2021-08-30 11:32:01 +0800

    news

--- src/pages/news/[id].js
+++ src/pages/news/[id].js
@@ -4,12 +4,14 @@ import {callGet} from '@api/api';
 import Footer from '../../components/Footer';
 import {useRouter} from 'next/router';
 import {useState, useEffect} from 'react';
-import {Image} from 'antd';
+import {Image, Input, Divider, Button} from 'antd';
+import {EyeOutlined, HeartFilled, HeartOutlined, ShareAltOutlined} from '@ant-design/icons';
 
 const NewsId = () => {
   const router = useRouter();
   const {id} = router.query;
   const [data, setdata] = useState(null);
+  const {TextArea} = Input;
 
   const fetchData = async () => {
     // setisLoading(true);
@@ -33,16 +35,76 @@ const NewsId = () => {
   data != null ? console.log(data, 'dataaa') : null;
 
   return (
-    <div>
+    <div style={{position: 'relative'}}>
+      <p style={{position: 'absolute', zIndex: '2', left: '794px', top: '40px', fontSize: '60px', width: '481px', height: '142', color: '#DEE2E9',
+        textAlign: 'right', fontWeight: 'bold', letterSpacing: '-0.5px'}}>
+        {data && data.title}</p>
       <Image
         preview={false}
         style={{zIndex: '1'}}
+        width= {'100%'}
         src="../../images/news.png"
       />
-      <div style={{position: 'absolute', top: '318px'}}>
+
+
+      <p
+        style={{
+          position: 'absolute',
+          left: '180px',
+          top: '271px',
+          // backgroundColor: 'red',
+          zIndex: '2',
+          color: 'blue',
+        }}
+      >
+        <EyeOutlined />
+
+      </p>
+      <p
+        style={{
+          position: 'absolute',
+          left: '200px',
+          top: '275px',
+          zIndex: '2',
+          color: 'blue',
+          fontSize: '12px',
+          fontStyle: 'italic',
+
+        }}
+      >
+                11200
+      </p>
+      <p
+        style={{
+          position: 'absolute',
+          left: '260px',
+          top: '271px',
+          // backgroundColor: 'red',
+          zIndex: '2',
+          color: 'blue',
+        }}
+      >
+        <HeartFilled />
+
+      </p>
+      <p
+        style={{
+          position: 'absolute',
+          left: '280px',
+          top: '275px',
+          zIndex: '2',
+          color: 'blue',
+          fontSize: '12px',
+          fontStyle: 'italic',
+
+        }}
+      >
+                110
+      </p>
+      <div style={{position: 'absolute', top: '250px'}}>
         <div
           style={{
-            top: '318px',
+
             zIndex: '2',
             marginLeft: '10%',
             width: '1110px',
@@ -52,31 +114,50 @@ const NewsId = () => {
             backgroundColor: 'white',
           }}
         >
-          <div
-            style={{fontSize: '24px', marginTop: '64px', color: '#35446D'}}
-          >
-            {data && data.title}
-          </div>
-          <div>
+
+          <div style={{marginTop: '64px', alignItems: 'center'}}>
             {data != [] && data != null ? (
               <Image
                 preview={false}
                 width={600}
                 height={350}
+
                 src={'https://uparking.mn' + data.image}
               />
+
             ) : null}
-          </div>
-          <div
-            style={{width: '540px', alignItems: 'center', color: '#141A29'}}
-          >
-            {data != [] && data ? parse(` ${data.content}`) : null}
+
+            <div
+              style={{width: '540px', alignItems: 'center', color: '#141A29', marginBottom: '50px'}}
+            >
+              {data != [] && data ? parse(` ${data.content}`) : null}
+            </div>
+            <div style={{display: 'flex', cursor: 'pointer', justifyContent: 'space-around', width: '300px', marginLeft: '20%'}}>
+              <div style={{display: 'flex', width: '100px', justifyContent: 'space-around'}}>
+                <div style={{color: 'blue'}}><HeartOutlined /></div>
+                <div style={{fontSize: '14px', color: '#0013D4', marginTop: '2px', fontWeight: 'bold'}}>Таалагдлаа </div>
+              </div>
+              <div style={{display: 'flex', width: '100px', justifyContent: 'space-around'}}>
+                <div style={{color: 'blue'}}><ShareAltOutlined /></div>
+                <div style={{fontSize: '14px', color: '#0013D4', marginTop: '2px', fontWeight: 'bold'}}>Хуваалцах</div>
+              </div>
+            </div>
+            <div style={{width: '600px', height: '220px', top: '500px', margin: '32px'}}>
+              <Input bordered={false} placeholder="Таны нэр" />
+              <Divider />
+              <TextArea bordered={false} rows={4} autoSize={{minRows: 3, maxRows: 5}} placeholder="Сэтгэгдэл бичих" />
+              <Button style={{marginLeft: '70%', borderRadius: '8px'}} type="primary" >Сэтгэгдэл илгээх</Button>
+            </div>
+
+
           </div>
         </div>
         <div>
+
           <Footer />
         </div>
       </div>
+
 
       {/* <div style={{ position: "absolute", zIndex: "2" }}>
         <Footer />
--- src/pages/park/index.js
+++ src/pages/park/index.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react';
 import {callGet} from '@api/api';
+import {useRouter} from 'next/router';
 import {showMessage} from '../../utils/message';
 import {
   Col,
@@ -87,6 +88,8 @@ const Dashboard = () => {
   const [searchedData, setSearchedData] = useState([]);
   const [dataSource, setDataSource] = useState([]);
   const [parkingUpDownArrow, setParkingUpDownArrow] = useState(false);
+  const [transfer, setTransfer] = useState(null);
+  const router = useRouter();
   const [defaultCenter, setDefaultCenter] = useState({
     lat: 47.91909306508191,
     lng: 106.91761127921768,
@@ -120,6 +123,9 @@ const Dashboard = () => {
   const showDrawer = async (item) => {
     const res = await callGet(
       `/parkingspace?parkingSpaceId=${item.park.parkingSpaceId}`,
+
+      setTransfer(item.park.parkingSpaceId),
+
     );
     if (!res || res === undefined) {
       showMessage(messageType.FAILED.type, defaultMsg.dataError);
@@ -210,6 +216,17 @@ const Dashboard = () => {
   const onChangeEndDate = (date) => {
     setEndDate(date);
   };
+  const onTransfer = (id) => {
+    console.log(id, 'ehnii id');
+    id &&
+      router.push({
+        pathname: 'park/payment',
+        query: {
+          id: id,
+        },
+      });
+  };
+
 
   const onFinish = async (values) => {
     console.log(values, 'values');
@@ -897,7 +914,7 @@ const Dashboard = () => {
                     </Button>
                   </Col>
                   <Col span={12}>
-                    <Button type="primary" size={'large'} block>
+                    <Button type="primary" size={'large'} block onClick={() => onTransfer(transfer)} >
                       Төлбөр төлөх
                     </Button>
                   </Col>
